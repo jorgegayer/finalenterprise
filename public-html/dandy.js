5d002822cceb1d76c9654dcb20ad607ab30bbc11
jorgegayer <jorgegayer@gmail.com>
2022-04-13 08:57:19 -0600

    updating dandy

---
+++ public-html/dandy.js
@@ -0,0 +1,9 @@
+function openBeers() {
+  window.open("beers.html", "_self");
+  console.log("Opening beers");
+}
+
+function openMenu() {
+  window.open("./Dandy-Menu.html", "_self");
+  console.log("Opening Menu");
+}
